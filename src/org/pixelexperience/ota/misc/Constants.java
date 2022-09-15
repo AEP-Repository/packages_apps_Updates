5e6ffe701746fcc54c0e6883cffb7380bf0aa584
AEP-Repository <aep.repository@gmail.com>
2022-09-15 11:09:00 +0700

    ota: Support unofficial build

--- src/org/pixelexperience/ota/misc/Constants.java
+++ src/org/pixelexperience/ota/misc/Constants.java
@@ -22,7 +22,6 @@ public final class Constants {
     public static final String PREF_MOBILE_DATA_WARNING = "pref_mobile_data_warning";
     public static final String UNCRYPT_FILE_EXT = ".uncrypt";
     public static final String PROP_BUILD_DATE = "org.pixelexperience.build_date_utc";
-    public static final String PROP_BUILD_TYPE = "org.pixelexperience.build_type";
     public static final String PROP_RECOVERY_UPDATE = "persist.sys.recovery_update";
     public static final String PREF_CURRENT_PERSISTENT_STATUS = "current_persistent_status";
     public static final String PREF_INSTALLING_AB_ID = "installing_ab_id";
@@ -30,9 +29,8 @@ public final class Constants {
     static final String PROP_AB_DEVICE = "ro.build.ab_update";
     static final String PROP_DEVICE = "org.pixelexperience.device";
     static final String PROP_BUILD_VERSION = "org.pixelexperience.version";
-    static final String OTA_URL = "https://download.pixelexperience.org/ota_v5/%s/%s";
-    static final String OTA_CI_URL = "https://download.pixelexperience.org/ota_ci/%s/%s";
-    static final String MAINTAINER_URL = "https://download.pixelexperience.org/team/%s";
-    static final String DOWNLOAD_WEBPAGE_URL = "https://download.pixelexperience.org/changelog/%s/%s";
+    static final String OTA_URL = "https://raw.githubusercontent.com/a3project/ota/experience/rosy.json";
+    static final String MAINTAINER_URL = "https://github.com/a3project";
+    static final String DOWNLOAD_WEBPAGE_URL = "https://raw.githubusercontent.com/a3project/ota/pixelos/changelog.txt";
     static final String EXPORT_PATH = "PixelExperience-Updates/";
 }
